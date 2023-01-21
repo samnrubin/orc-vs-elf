c443a856affcb52cf109d0e55aab49bf0cbf9737
Sam Rubin <samnrubin@gmail.com>
2023-01-21 15:32:32 -0800

    UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 import {
   Flex,
   Box,
-  HStack
+  HStack,
+  Text,
+  VStack,
+  Heading
 } from '@chakra-ui/react';
 
+import { useState } from 'react';
+
+const testMoves = ["Test1", "Test2", "Test3"]
+const testLastMove = "The orc attacks the elf"
+
 function App() {
+  // States are "choose" for choosing a move, "display" for displaying the moves, and "end" for the end of the game
+  const [state, setState] = useState("display");
   return (
     <Flex
       width="100vw"
@@ -23,8 +32,21 @@ function App() {
       <CharacterDisplay character="Orc" />
       <Flex
         width="50%"
+        justifyContent={"center"}
       >
-        Main display
+        <Flex
+          width="80%"
+          height="100%"
+          flexDirection={"column"}
+          bg = "#ffffffa6"
+          color="black"
+        >
+          {state === "choose" ?
+          <ChooseMove attacker="Orc" attackerOptions={testMoves} defenderOptions={testMoves} lastMove={testLastMove} player="Elf" />
+          :
+          <DisplayResults success={true} reasoning={"The orc rolled a 5 and the elf rolled a 4"} moveDescription={"The orc attacks the elf"} attacker={"Orc"} />
+          }
+        </Flex>
       </Flex>
       <CharacterDisplay character="Elf" />
     </Flex>
@@ -57,28 +79,87 @@ const CharacterDisplay = ({character}) => {
 // Health display shows three side by side hearts in a flex box split into three
 const HealthDisplay = ({heartNum}) => {
   return (
-    <HStack>
+    <HStack
+      justifyContent={"center"}
+    >
       <Box
         as="img"
         src={process.env.PUBLIC_URL + "/heart.png"}
         maxHeight = "100%"
-        maxW={"33.3%"}
+        maxW={"30%"}
         visibility={heartNum >= 1 ? "visible" : "hidden"}
       />
       <Box
         as="img"
         src={process.env.PUBLIC_URL + "/heart.png"}
         maxHeight = "100%"
-        maxW={"33.3%"}
+        maxW={"30%"}
         visibility={heartNum >= 2 ? "visible" : "hidden"}
       />
       <Box
         as="img"
         src={process.env.PUBLIC_URL + "/heart.png"}
         maxHeight = "100%"
-        maxW={"33.3%"}
+        maxW={"30%"}
         visibility={heartNum >= 3 ? "visible" : "hidden"}
       />
     </HStack>
+  )
+}
+
+const ChooseMove = ({attacker, attackerOptions, defenderOptions, lastMove, player}) => {
+  return (
+    <>
+      <Flex
+        justifyItems={"center"}
+        flexDirection={"column"}
+      >
+        <Heading>Current attacker: {attacker}</Heading>
+        <Text>You are playing as the {player}</Text>
+      </Flex>
+      <Flex
+        alignItems={"center"}
+        flexDirection={"column"}
+      >
+        <Heading>Last move:</Heading>
+        <Text>{lastMove}</Text>
+      </Flex>
+      <MoveDisplay title="Attacker" moves={attackerOptions} />
+      <MoveDisplay title="Defender" moves={defenderOptions} />
+    </>
+  )
+}
+
+const MoveDisplay = ({title, moves}) => {
+  return (
+    <VStack>
+      <Heading>{title} moves:</Heading>
+      {moves.map((move, index) => (
+        <Text>{index + 1}. {move}</Text>
+      ))}
+    </VStack>
+
+  )
+}
+
+const DisplayResults = ({success, reasoning, moveDescription, attacker}) => {
+  let output = success ? "succeeded!" : "failed!"
+  return (
+    <>
+      <Flex
+        alignItems={"center"}
+        flexDirection={"column"}
+      >
+        <Heading>The {attacker}'s move {output}</Heading>
+        <Box as="span" fontSize={"150%"}>Reasoning:</Box> <Text>{reasoning}</Text>
+      </Flex>
+      <Flex
+        alignItems={"center"}
+        flexDirection={"column"}
+      >
+        <Heading>Move</Heading>
+        <Text>{moveDescription}</Text>
+      </Flex>
+    </>
   )
 }
