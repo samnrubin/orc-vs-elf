109e70b75f0fdb826e9b53e91405ccd5a759a136
mmxmb <22385215+mmxmb@users.noreply.github.com>
2023-01-21 16:36:52 -0800

    Simplify game state transitions

--- backend/app/main.py
+++ backend/app/main.py
@@ -47,58 +47,61 @@ class Player():
         self.taken_action = action
 
 
-class TurnStatus(Enum):
-    IN_PROGRESS = "in progress"
-    END = "end"
 
-class Turn():
-    def __init__(self):
-        self.status = TurnStatus.IN_PROGRESS
-        self.reason = ""
-        self.outcome = ""
-
-    def end(self, reason, outcome):
-        self.status = TurnStatus.END
-        self.reason = reason
-        self.outcome = outcome
-
-class GameStatus(Enum):
+class GameStatus():
     IN_PROGRESS = "in progress"
     PLAYER1WON = "player1 won"
     PLAYER2WON = "player2 won"
     DRAW = "draw"
 
 class Game:
-    def __init__(self, player1: Player, player2: Player):
-        self.player1 = player1
-        self.player2 = player2
+    def __init__(self, player1name, player2name):
         self.player1attack = True
+        self.player1 = Player(player1name, 3)
+        self.player2 = Player(player2name, 3)
         self.turn_num = 0
-        self.turn = Turn()
         self.game_status = GameStatus.IN_PROGRESS
+        self.description = ""
+        self.outcome = ""
+        self.reason = ""
+        self.player1.available_actions = get_actions(player1name, True)
+        self.player2.available_actions = get_actions(player2name, False)
 
     def next_turn(self):
-        if self.player1.took_action and self.player2.took_action:
-            self.player1.took_action = False
-            self.player2.took_action = False
-            self.player1.available_actions = []
-            self.player2.available_actions = []
-            self.player1attack = not self.player1attack
+        if self.player1.taken_action and self.player2.taken_action:
+            self.current_turn_result()
+            self.player1.taken_action = None
+            self.player2.taken_action = None
             self.turn_num += 1
-            self.turn = Turn()
+            self.description = "new desc"
+            self.outcome = "new outcome"
+            self.reason = "new reason"
             if self.player1.health <= 0 and self.player2.health <= 0:
                 self.game_status = GameStatus.DRAW
             if self.player1.health <= 0:
                 self.game_status = GameStatus.PLAYER2WON
             if self.player2.health <= 0:
                 self.game_status = GameStatus.PLAYER1WON
-    
+            self.player1attack = not self.player1attack
+            self.player1.available_actions = get_actions(self.player1.name, self.player1attack)
+            self.player2.available_actions = get_actions(self.player2.name, not self.player1attack)
 
-game = Game(Player("elf", 3), Player("orc", 3))
+    def current_turn_result(self):
+        # send taken action by both player to the prompt and get back description, outcome and reason
+        pass
+
+
+def get_actions(player, is_attack):
+    if is_attack:
+        return random.sample(attack_moves, 3)
+    else:
+        return random.sample(defense_moves, 3)
+
+game = Game("elf", "orc")
 
 @app.post("/new_game")
 async def new_game():
-    game = Game(Player("elf", 3), Player("orc", 3))
+    game = Game("elf", "orc")
 
 
 @app.post("/take_action")
@@ -117,20 +120,11 @@ async def take_action(data: dict):
         game.player2.take_action(Action(action))
     else:
         return {"error": f"player not found, available players {game.player1.name}, {game.player2.name}"}
-    
+    if game.player1.taken_action and game.player2.taken_action:
+        game.next_turn()
+
+
 
 @app.get("/game_state")
 async def game_state():
-    if not game.player1.taken_action and not game.player1.available_actions:
-        if game.player1attack:
-            game.player1.available_actions = random.sample(attack_moves, 3)
-        else:
-            game.player1.available_actions = random.sample(defense_moves, 3)
-    if not game.player2.taken_action and not game.player2.available_actions:
-        if game.player1attack:
-            game.player2.available_actions = random.sample(defense_moves, 3)
-        else:
-            game.player2.available_actions = random.sample(attack_moves, 3)
-    if game.player1.taken_action and game.player2.taken_action:
-        game.turn.end("both players took action", "outcome")
     return game.__dict__
