a0d182ef08c64060e7e1536ea409a65d680978fe
mmxmb <22385215+mmxmb@users.noreply.github.com>
2023-01-21 19:07:14 -0800

    Implement full state transition

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from gpt import generate_moves
+from gpt import generate_moves, evaluate_moves, generate_next_move
 from enum import Enum
 import random
 from fastapi.middleware.cors import CORSMiddleware
@@ -71,19 +71,18 @@ class Game:
         self.player2 = Player(player2name, 3)
         self.turn_num = 0
         self.game_status = GameStatus.IN_PROGRESS
-        self.description = ""
-        self.outcome = ""
-        self.reason = ""
+        self.last_move = None
+        self.result = None
+        self.reasoning = None
         self.player1.available_actions = generate_moves(True, player1name)
         self.player2.available_actions = generate_moves(False, player2name)
 
     def next_turn(self):
         if self.player1.taken_action and self.player2.taken_action:
-            self.current_turn_result()
+            last_player1_move = self.player1.available_actions[self.player1.taken_action.value]
+            last_player2_move = self.player2.available_actions[self.player2.taken_action.value]
+            self.current_turn_result(last_player1_move, last_player2_move)
             self.turn_num += 1
-            self.description = "new desc"
-            self.outcome = "new outcome"
-            self.reason = "new reason"
             if self.player1.health <= 0 and self.player2.health <= 0:
                 self.game_status = GameStatus.DRAW
             if self.player1.health <= 0:
@@ -91,16 +90,27 @@ class Game:
             if self.player2.health <= 0:
                 self.game_status = GameStatus.PLAYER1WON
             self.player1attack = not self.player1attack
-            last_player1_move = self.player1.available_actions[self.player1.taken_action.value]
-            last_player2_move = self.player2.available_actions[self.player2.taken_action.value]
             self.player1.available_actions = generate_moves(self.player1attack, self.player1.name, last_player1_move)
             self.player2.available_actions = generate_moves(not self.player1attack, self.player2.name, last_player2_move)
             self.player1.taken_action = None
             self.player2.taken_action = None
 
-    def current_turn_result(self):
-        # send taken action by both player to the prompt and get back description, outcome and reason
-        pass
+    def current_turn_result(self, player1move, player2move):
+        attacker = self.player1.name if self.player1attack else self.player2.name
+        print(f"evaluating moves:\n attacker - {attacker}\n elfmove - {player1move}\n orcmove - {player2move}\n")
+        moves_eval = evaluate_moves(attacker, player1move, player2move, self.last_move)
+        self.result = moves_eval["result"]
+        self.reasoning = moves_eval["reasoning"]
+        print(f"evaluating next move:\n attacker - {attacker}\n elfmove - {player1move}\n orcmove - {player2move}\n result - {self.result}\n reasoning - {self.reasoning}\n")
+        self.last_move = generate_next_move(attacker, player1move, player2move, self.result, self.reasoning, self.last_move)
+
+    def update_health(self, attacker, success):
+        if attacker == self.player1.name and success:
+            self.player2.health -= 1
+        elif attacker == self.player2.name and success:
+            self.player1.health -= 1
+
+
 
 
 def get_actions(player, is_attack):
